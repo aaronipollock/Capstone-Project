db1fa5b6fafee34df9219f664de7f1c62794dc54
Aaron <aaronipollock@gmail.com>
2024-07-19 21:39:01 -0700

    logout button

---
+++ react-vite/src/redux/notebooksSlice.js
@@ -0,0 +1,18 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const notebooksSlice = createSlice({
+//     name: 'notebooks',
+//     initialState: [],
+//     reducers: {
+//         setNotebooks: (state, action) => {
+//             return action.payload;
+//         },
+//         dumpNotebook: () => {
+//             return [];
+//         }
+//     }
+// });
+
+// export const { setNotebooks, dumpNotebooks } = notebooksSlice.actions;
+// export default notebooksSlice.reducer;
+
---
+++ react-vite/src/redux/notesSlice.js
@@ -0,0 +1,19 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const notesSlice = createSlice({
+//     name: 'notes',
+//     initialState: [],
+//     reducers: {
+//         setNotes: {
+//             setNotes: (state, action) => {
+//                 return action.payload;
+//             },
+//             dumpNotes: () => {
+//                 return [];
+//             }
+//         }
+//     }
+// })
+
+// export const { setNotes, dumpNotes } = notesSlice.action;
+// export default notesSlice.reducer;
