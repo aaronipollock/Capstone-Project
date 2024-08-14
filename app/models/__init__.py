8cb566dc2941cc7e68f7e07dcce664a9173f48e3
Aaron <aaronipollock@gmail.com>
2024-08-14 16:26:32 -0700

    no  all

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,12 +3,12 @@ from .user import User
 from .notebook import Notebook, notebook_notes
 from .note import Note
 
-__all__= [
-    'db',
-    'environment',
-    'SCHEMA',
-    'User',
-    'Notebook',
-    'Note',
-    'notebook_notes',
-]
+# __all__= [
+#     'db',
+#     'environment',
+#     'SCHEMA',
+#     'User',
+#     'Notebook',
+#     'Note',
+#     'notebook_notes',
+# ]
