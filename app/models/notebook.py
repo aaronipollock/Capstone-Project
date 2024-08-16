71be3fb108dffe32246c8495c5be53286508132e
Aaron <aaronipollock@gmail.com>
2024-08-15 18:05:55 -0700

    prefix

--- app/models/notebook.py
+++ app/models/notebook.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import Table, Column, Integer, ForeignKey
 
-notebook_notes = Table('notebook_notes', db.Model.metadata,
+notebook_notes = Table(
+    add_prefix_for_prod('notebook_notes'), db.Model.metadata,
     Column('notebook_id', Integer, ForeignKey(add_prefix_for_prod('notebooks.id')), primary_key=True),
     Column('note_id', Integer, ForeignKey(add_prefix_for_prod('notes.id')), primary_key=True)
 )
