1d011ca7bf2f45f99573daec1572e16cdc3d7fca
Aaron <aaronipollock@gmail.com>
2024-12-18 14:02:46 -0800

    non-functional search bar

--- react-vite/src/components/Sidebar/Sidebar.jsx
+++ react-vite/src/components/Sidebar/Sidebar.jsx
@@ -6,19 +6,33 @@ import CreateNotebookModal from '../CreateNotebookModal'
 import CreateNoteModal from '../CreateNoteModal';
 import TagsModal from '../TagsModal';
 import { MdOutlineHome } from "react-icons/md";
-// import { FaRegStar } from "react-icons/fa";
 import { HiOutlineDocumentText } from "react-icons/hi";
-// import { FaRegCheckCircle } from "react-icons/fa";
 import { FiTag } from "react-icons/fi";
 import { PiNotebookBold } from "react-icons/pi";
+import { useState } from 'react';
 
 
 
 function Sidebar() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
     return (
         <div className="side-bar-container">
             <ProfileButton className="profile-button" />
             <div className="user-profile"></div>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                    className="search-input"
+                />
+            </div>
             <div>
                 <OpenModalButton
                     className="button notebook"
@@ -34,36 +48,19 @@ function Sidebar() {
                         modalComponent={<CreateNoteModal />}
                     />
                 </div>
-                {/* <div>
-                    <button className="button task">+Task</button>
-                    <button className="button event">+Event</button>
-                </div> */}
             </div>
             <div className="sidebar-menu-buttons">
                 <Link to="/users/current" className="link-menu-button">
                     <MdOutlineHome className="icon" />
-                    {/* <button className="menu-button home">Home</button> */}
                     <span>Home</span>
                 </Link>
-                {/* <Link to="/users/current" className="link-menu-button">
-                    <FaRegStar className="icon" />
-                    <span>Shortcuts</span> */}
-                {/* <button className="menu-button">Shortcuts</button> */}
-                {/* </Link> */}
                 <Link to="/notes" className="link-menu-button">
                     <HiOutlineDocumentText className='icon' />
                     <span>Notes</span>
-                    {/* <button className="menu-button notes">Notes</button> */}
                 </Link>
-                {/* <Link to="/users/current" className="link-menu-button"> */}
-                    {/* <FaRegCheckCircle className="icon" />
-                    <span>Tasks</span> */}
-                    {/* <button className="menu-button">Tasks</button> */}
-                {/* </Link> */}
                 <Link to="/notebooks" className="link-menu-button">
                     <PiNotebookBold className="icon" />
                     <span>Folios</span>
-                    {/* <button className="menu-button notebooks">Notebooks</button> */}
                 </Link>
                 <div>
                     <OpenModalButton
@@ -76,13 +73,6 @@ function Sidebar() {
                         modalComponent={<TagsModal />}
                     />
                 </div>
-                {/* <Link to="/users/current" className="link-menu-button">
-                    <FiTag className="icon" />
-                    <span>Tags</span>
-                </Link> */}
-                {/* <button className="menu-button">Tags</button> */}
-                {/* <button className="menu-button trash">Trash</button>
-                {/* <button className="menu-button upgrade"><strong>Upgrade</strong></button> */}
             </div>
         </div>
     )
